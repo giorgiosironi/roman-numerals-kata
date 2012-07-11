75916a4ed5691b9ad4c2b3a1ab102de382ff3577
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-07-11 14:43:31 +0200

    Configurability of roman cypher conversion functions

--- RomanNumeralsTest.php
+++ RomanNumeralsTest.php
@@ -1,55 +1,64 @@
 <?php
 
-function toRoman($number)
+function romanSystem($firstSymbol, $middleSymbol, $lastSymbol)
 {
-    $firstSymbol = 'I';
-    $symbols = array(1 => $firstSymbol, 5 => 'V', 10 => 'X');
-    $roman = '';
-    foreach ($symbols as $denomination => $symbol) {
-        if ($number == $denomination - 1) {
-            return $firstSymbol . $symbol;
+    return function($number) use ($firstSymbol, $middleSymbol, $lastSymbol) {
+        $symbols = array(1 => $firstSymbol,
+                         5 => $middleSymbol,
+                         10 => $lastSymbol);
+        $roman = '';
+        foreach ($symbols as $denomination => $symbol) {
+            if ($number == $denomination - 1) {
+                return $firstSymbol . $symbol;
+            }
         }
-    }
-    foreach (array_reverse($symbols, true) as $denomination => $symbol) {
-        while ($number >= $denomination) {
-            $roman .= $symbol;
-            $number -= $denomination;
-        } 
-    }
-    return $roman;
+        foreach (array_reverse($symbols, true) as $denomination => $symbol) {
+            while ($number >= $denomination) {
+                $roman .= $symbol;
+                $number -= $denomination;
+            } 
+        }
+        return $roman;
+    };
 }
 
 class RomanNumeralsTest extends PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->toRoman = romanSystem('I', 'V', 'X');
+    }
+
     public function toRoman($number)
     {
-        return toRoman($number);
+        $toRoman = $this->toRoman;
+        return $toRoman($number);
     }
 
     public function testTheISymbolCanBeRepeated()
     {
-        $this->assertEquals('I', toRoman(1));
-        $this->assertEquals('II', toRoman(2));
-        $this->assertEquals('III', toRoman(3));
+        $this->assertEquals('I', $this->toRoman(1));
+        $this->assertEquals('II', $this->toRoman(2));
+        $this->assertEquals('III', $this->toRoman(3));
     }
 
     public function testFiveHasItsOwnSymbol()
     {
-        $this->assertEquals('V', toRoman(5));
+        $this->assertEquals('V', $this->toRoman(5));
     }
 
     public function testSmallerSymbolsOnTheRightAreAdditive()
     {
-        $this->assertEquals('VI', toRoman(6));
+        $this->assertEquals('VI', $this->toRoman(6));
     }
 
     public function testSmallerSymbolsOnTheLeftAreSubtractive()
     {
-        $this->assertEquals('IV', toRoman(4));
+        $this->assertEquals('IV', $this->toRoman(4));
     }
 
     public function testTheVSymbolCannotBeRepeatedTwice()
     {
-        $this->assertEquals('IX', toRoman(9));
+        $this->assertEquals('IX', $this->toRoman(9));
     }
 }
