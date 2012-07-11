64111eb920fe16b5c9b2e17d7c96964fbb31b57e
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-07-11 11:22:27 +0200

    Failing test: 5 => V

--- RomanNumeralsTest.php
+++ RomanNumeralsTest.php
@@ -13,4 +13,9 @@ class RomanNumeralsTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('II', toRoman(2));
         $this->assertEquals('III', toRoman(3));
     }
+
+    public function testFiveIsConverted()
+    {
+        $this->assertEquals('V', toRoman(5));
+    }
 }
